347535e7043c97174df77295eac1578b3c21485f
Cameron Smith <smithc11@rpi.edu>
2023-09-18 16:39:30 +0000

    gem reconnection challenge problem

---
+++ gemReconnection/gemReconnection.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+usage="<model> <electronMass> <Lx> <Ly> <tEnd> <cellsX> <cellsY>"
+args=7
+[[ $# -ne $args ]] && echo "$0 $usage" && exit 1
+
+model=$1
+[[ "$model" != "5" && "$model" != "10" ]] && \
+  echo "model was set to \"$model\", valid options are 5 or 10" && \
+  exit 1
+
+luaScript=rt-${model}m-gem.lua
+original_luaScript=/export/gkeyllSoft/gemReconnection/$luaScript
+[[ ! -e "$original_luaScript" ]] && echo "luaScript $1 does not exist" && exit 1
+
+electronMass=$2
+
+#copy the script to the working directory
+cp $original_luaScript $luaScript
+
+sed -i "s/elcMass = ionMass\\/25.0/elcMass = ionMass\\/${electronMass}/" $luaScript
+
+#Lx = 25.6 * di0
+Lx=$3
+sed -i "s/Lx = 25.6 \* di0/Lx = ${Lx} \* di0/" $luaScript
+#Ly = 12.8 * di0
+Ly=$4
+sed -i "s/Ly = 12.8 \* di0/Ly = ${Ly} \* di0/" $luaScript
+
+#tEnd = 25.0/OmegaCi0
+tEnd=$5
+sed -i "s/tEnd = 25.0\\/OmegaCi0/tEnd = ${tEnd}\\/OmegaCi0/" $luaScript
+
+#cells = {64, 32}
+cellsX=$6
+cellsY=$7
+sed -i "s/cells = {64, 32}/cells = {${cellsX}, ${cellsY}}/" $luaScript
+
+diff $luaScript $original_luaScript #DEBUG
+
+/export/gkeyllSoft/bin/gkyl $luaScript
---
+++ gemReconnection/plotFV.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+import sys
+sys.path.append("/export/gkeyllSoft/postgkyl")
+
+import postgkyl as pg
+
+run = Path.cwd()
+pfx = "rt-5m-gem"
+frame = 0
+
+filename = run / f"{pfx}_field_{frame}.bp"
+filename = str(filename)
+
+gdata = pg.GData(filename)
+
+vals = gdata.getValues() # cell-center values, shape is Ny * Nx * Ncomponents
+grid = gdata.getGrid() # cell corner coordinates
+ndim = gdata.getNumDims() # number of spatial dimensions
+
+print(vals.shape)
+print([g.shape for g in grid])
+print(ndim)
+
+assert len(vals.shape) == ndim+1 and len(grid) == ndim
+
+icomp = 3
+compName = r'$E_z^2$'
+fileName = "E_z.png"
+
+my_vals = vals[..., icomp]
+x, y = grid
+
+fig, ax = plt.subplots()
+
+im = ax.pcolormesh(x, y, my_vals.T)
+fig.colorbar(im, ax=ax)
+
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_title(compName)
+ax.set_aspect(1)
+
+plt.savefig(fileName)
