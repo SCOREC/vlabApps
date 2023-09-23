a024361bed57c2a22c8838ddf7e2bb553a8d6796
Cameron Smith <smithc11@rpi.edu>
2023-09-23 03:39:43 +0000

    buneman: print hash, need full path

--- buneman/buneman.sh
+++ buneman/buneman.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+
+#there should be a better way...
+repoDir=/export/vlabApps/buneman/
+cd $repoDir 
+gitHash=$(git rev-parse HEAD)
+echo "git hash: $gitHash"
+cd -
+
 luaScript=buneman-5m.lua
 eval "$(conda shell.bash hook)"
 conda activate /export/gkeyllSoft/postgkyl
-python generate-buneman-5m.py $luaScript "$@"
+python $repoDir/generate-buneman-5m.py $luaScript "$@"
 [[ ! -e $luaScript ]] && echo "Lua script $luaScript does not exist" && exit 1
 /export/gkeyllSoft/bin/gkyl $luaScript
