3b023e3b61ccdb58cc200a8dd78b1a832df6d53a
Cameron Smith <smithc11@rpi.edu>
2024-02-20 17:33:10 +0000

    fix typo

--- lapd/lapd.sh
+++ lapd/lapd.sh
@@ -7,7 +7,7 @@ gitHash=$(git rev-parse HEAD)
 echo "git hash: $gitHash"
 cd -
 
-luaScript=$repDir/LAPD3D5Mg2_coarseGrid.lua
+luaScript=$repoDir/LAPD3D5Mg2_coarseGrid.lua
 eval "$(conda shell.bash hook)"
 conda activate /export/gkeyllSoft/postgkyl
 [[ ! -e $luaScript ]] && echo "Lua script $luaScript does not exist" && exit 1
