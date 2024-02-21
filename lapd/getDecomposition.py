9c2bc4a55224081b0b973c8c216bb2bdbcd460c2
Cameron Smith <smithc11@rpi.edu>
2024-02-21 03:33:21 +0000

    better

--- lapd/getDecomposition.py
+++ lapd/getDecomposition.py
@@ -8,10 +8,10 @@ nz=int(sys.argv[2])
 cellsPerRank=1800
 z2r=3
 
-ncells=nr*nr*nz
-ranks=np.ceil(ncells/cellsPerRank)
-ncz=np.ceil(ranks/z2r)
-ncr=np.ceil(np.sqrt(ranks/ncz))
-totRanks=ncr*ncr*ncz
-print("target ranks {} ncz {} ncr {} ranks {}".format(ranks, ncz, ncr, totRanks))
+cells = nr * nr * nz
+ncr = ((cells / cellsPerRank) / z2r) ** (1. / 3)
+ncz = 3 * ncr
+totRanks = ncr * ncr * ncz
+print("target ranks {} ncz {} ncr {} ranks {} cellsPerRank {}".format(cells / cellsPerRank, ncz,
+    ncr, totRanks, cells / totRanks))
 
