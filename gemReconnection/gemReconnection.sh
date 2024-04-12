3a8dffc775b0eaf171d771ce87b6e5cd7c3c55b9
Cameron Smith <smithc11@rpi.edu>
2024-04-12 13:18:30 -0400

    gemReconnetion: use env vars for bin and conda paths

--- gemReconnection/gemReconnection.sh
+++ gemReconnection/gemReconnection.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-#there should be a better way...
-repoDir=/export/vlabApps/gemReconnection/
+set -x
+echo $GKYL
+echo $PGKYL_CONDA_ENV
+echo $CONDA
+set +x
+[[ ! -e "$GKYL" ]] && echo "path to gkyl executable $GKYL does not exist" && exit 1
+[[ ! -d "$PGKYL_CONDA_ENV" ]] && echo "path to pgkyl conda environment $PGKYL_CONDA_ENV does not exist or is not a directory" && exit 1
+[[ ! -e "$CONDA" ]] && echo "path to conda binary $CONDA does not exist" && exit 1
+
+repoDir=$(dirname $(readlink -f $0))
 cd $repoDir 
 gitHash=$(git rev-parse HEAD)
 echo "git hash: $gitHash"
@@ -45,4 +53,12 @@ sed -i "s/cells = {64, 32}/cells = {${cellsX}, ${cellsY}}/" $luaScript
 
 diff $luaScript $original_luaScript #DEBUG
 
-/export/gkeyllSoft/bin/gkyl $luaScript
+srun -n ${SLURM_NPROCS} $GKYL $luaScript
+
+## post processing
+eval "$(${CONDA} shell.bash hook)"
+conda activate $PGKYL_CONDA_ENV
+pgkyl rt-*m-gem_field_1.bp sel -c 0,1,2,3,4,5 pl -x x -y y --diverging -a --figsize 12,3 --saveas 'fields.png'
+pgkyl rt-*m-gem_elc_1.bp pl -x x -y y -a --figsize 12,3 --saveas 'electrons.png'
+pgkyl rt-*m-gem_ion_1.bp pl -x x -y y -a --figsize 12,3 --saveas 'ions.png'
+python $repoDir/plotFV.py
